f55d4b71ff1f007cc3b8d5d1d1c71e11fa1e90b9
Dmytro Zhyltsov <zkarman@mail.ru>
2021-02-22 16:38:35 +0200

    chore: add React.js app

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>Hello!</div>;
+}
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
